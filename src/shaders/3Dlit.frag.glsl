fc2b8aea9f33a64fca75ca4720527a13c6e4133c
ABaker1343 <billybo1234@outlook.com>
2023-05-04 21:54:52 +0000

    shadows are looking better

--- src/shaders/3Dlit.frag.glsl
+++ src/shaders/3Dlit.frag.glsl
@@ -11,19 +11,14 @@ uniform sampler2D in_shadowMap;
 float shadowCalc(vec4 pointLightSpace) {
     vec3 proj = pointLightSpace.xyz / pointLightSpace.w;
     proj = proj * 0.5 + 0.5;
-    
-    return pointLightSpace.z > texture(in_shadowMap, proj.xy).z ? 0.0 : 1.0;
+    float depthOnMap = texture(in_shadowMap, proj.xy).r;
+
+    float depthBias = 0.05;
+
+    return proj.z > depthOnMap + depthBias ? 0.0 : 1.0;
 }
 
 void main() {
-
-    float shadow = shadowCalc(frag_lightspace);
-
-    fragColor = texture(in_texture, frag_tex) * shadow;
-
-    vec3 proj = frag_lightspace.xyz / frag_lightspace.w;
-    proj = proj * 0.5 + 0.5;
-    float depthOnMap = texture(in_shadowMap, proj.xy).r;
-    fragColor = vec4(proj.z, proj.z, proj.z, 1.0);
-    //fragColor.w = 1.0;
+    fragColor = texture(in_texture, frag_tex) * shadowCalc(frag_lightspace);
+    fragColor.w = 1.0;
 }
